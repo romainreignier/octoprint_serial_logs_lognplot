69d2ebae83a3c57e2f38c6d97e746ef94f4fd4a0
Romain Reignier <rom.reignier@gmail.com>
2020-03-30 12:21:32 +0200

    Read file line by line

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,56 @@
-extern crate chrono;
-extern crate clap;
-extern crate lognplot;
-extern crate regex;
-
 use chrono::prelude::*;
-use clap::{App, Arg};
+use clap::{crate_version, App, Arg};
 use lognplot::net::TcpClient;
 use regex::Regex;
 use std::fs::File;
-use std::io::Read;
+use std::io::{prelude::*, BufReader};
 
 fn main() {
     let matches = App::new("octoprint_serial_logs_lognplot")
-                          .version("1.0")
+                          .version(crate_version!())
                           .about("Read an Octoprint serial log file and send temperature data to a running lognplot instance to plot them.")
                           .arg(Arg::with_name("serial_logs")
                                .help("The serial_logs file to use")
                                .required(true)
                                .index(1))
                           .get_matches();
+    // Get filename
     let filename = matches.value_of("serial_logs").unwrap();
-    let mut f = File::open(filename).expect("file not found");
-    let mut content = String::new();
-    // Warning, load all file in memory...
-    f.read_to_string(&mut content)
-        .expect("something went wrong reading the file");
-    let re = match Regex::new(r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3}) - Recv: ok T:([0-9]*[.]?[0-9]+) [[:punct:]]([0-9]*[.]?[0-9]+) B:([0-9]*[.]?[0-9]+) [[:punct:]]([0-9]*[.]?[0-9]+)") {
-      Ok(r) => r,
-      Err(e) => {
-        println!("Could not compile regex: {}", e);
-        std::process::exit(1);
-      }
-    };
-
-    let mut client = match TcpClient::new("127.0.0.1:12345") {
-        Ok(client) => client,
-        Err(_e) => {
-            println!("Could not connect to lognplot server. Is it running?");
-            std::process::exit(1);
+    // Open the file
+    let file = File::open(filename).expect("file not found");
+    // Create the regex
+    let re = Regex::new(r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3}) - Recv: ok T:([0-9]*[.]?[0-9]+) [[:punct:]]([0-9]*[.]?[0-9]+) B:([0-9]*[.]?[0-9]+) [[:punct:]]([0-9]*[.]?[0-9]+)").expect("Could not compile regex");
+    // Create the lognplot client
+    let mut client = TcpClient::new("127.0.0.1:12345")
+        .expect("Could not connect to lognplot server. Is it running?");
+    // Create a buffered reader
+    let reader = BufReader::new(file);
+    // Read the file line by line
+    for line in reader.lines().map(|l| l.unwrap()) {
+        // Apply regex
+        match re.captures(&line) {
+            Some(caps) => {
+                let stamp = NaiveDateTime::parse_from_str(&caps[1], "%Y-%m-%d %H:%M:%S,%f")
+                    .expect("Failed to parse date")
+                    .timestamp();
+                let extruder_temp: f64 = caps[2].parse().unwrap();
+                let extruder_setpoint: f64 = caps[3].parse().unwrap();
+                let bed_temp: f64 = caps[4].parse().unwrap();
+                let bed_setpoint: f64 = caps[5].parse().unwrap();
+                client
+                    .send_sample("extruder temp", stamp as f64, extruder_temp)
+                    .unwrap();
+                client
+                    .send_sample("extruder setpoint", stamp as f64, extruder_setpoint)
+                    .unwrap();
+                client
+                    .send_sample("bed temp", stamp as f64, bed_temp)
+                    .unwrap();
+                client
+                    .send_sample("bed setpoint", stamp as f64, bed_setpoint)
+                    .unwrap();
+            }
+            None => (),
         }
-    };
-
-    for cap in re.captures_iter(&content) {
-        let stamp = NaiveDateTime::parse_from_str(&cap[1], "%Y-%m-%d %H:%M:%S,%f")
-            .expect("Failed to parse date")
-            .timestamp();
-        let extruder_temp: f64 = cap[2].parse().unwrap();
-        let extruder_setpoint: f64 = cap[3].parse().unwrap();
-        let bed_temp: f64 = cap[4].parse().unwrap();
-        let bed_setpoint: f64 = cap[5].parse().unwrap();
-        client
-            .send_sample("extruder temp", stamp as f64, extruder_temp)
-            .unwrap();
-        client
-            .send_sample("extruder setpoint", stamp as f64, extruder_setpoint)
-            .unwrap();
-        client
-            .send_sample("bed temp", stamp as f64, bed_temp)
-            .unwrap();
-        client
-            .send_sample("bed setpoint", stamp as f64, bed_setpoint)
-            .unwrap();
     }
 }
